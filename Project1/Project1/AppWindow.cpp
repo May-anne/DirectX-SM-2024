54ff96b01989e6ab96350853d48cf6b4307dd321
Arthur Xavier <101990249+arthurcxavier@users.noreply.github.com>
2024-06-18 22:52:22 -0300

    Upload de projeto AnimacaoDirectX
    O projeto foi construido como forma de aprendizado da api DirectX 11. Consiste em um quadrado que fica alternando sua posição entre eixos, sumindo quando fica totalmente projetado sobre o eixo XZ e aparecendo quando projetado sobre o eixo XY

---
+++ Project1/Project1/AppWindow.cpp
@@ -0,0 +1,125 @@
+#include "AppWindow.h"
+#include <Windows.h>
+
+
+struct vec3
+{
+	float x, y, z;
+};
+
+struct vertex
+{
+	vec3 position;
+	vec3 position1;
+	vec3 color;
+	vec3 color1;
+};
+
+
+__declspec(align(16))
+struct constant
+{
+	float m_angle;
+};
+
+
+AppWindow::AppWindow()
+{
+}
+
+
+AppWindow::~AppWindow()
+{
+}
+
+void AppWindow::onCreate()
+{
+	Window::onCreate();
+	GraphicsEngine::get()->init();
+	m_swap_chain=GraphicsEngine::get()->createSwapChain();
+
+	RECT rc = this->getClientWindowRect();
+	m_swap_chain->init(this->m_hwnd, rc.right - rc.left, rc.bottom - rc.top);
+
+	vertex list[] = 
+	{
+		//X - Y - Z
+		{-0.5f,-0.5f,0.0f,    0.5f,0.5f,0.0f,   0,0,0,  0,1,0 }, // POS1
+		{-0.5f,0.5f,0.0f,     -0.5f,1.0f,0.0f,    1,1,0,  0,1,1 }, // POS2
+		{ 0.5f,-0.5f,0.0f,     1.0f,-0.5f,0.0f,   0,0,1,  1,0,0 },// POS2
+		{ 0.5f,0.5f,0.0f,      -0.5f,-0.5f,0.0f,    1,1,1,  0,0,1 }
+	};
+
+	m_vb=GraphicsEngine::get()->createVertexBuffer();
+	UINT size_list = ARRAYSIZE(list);
+
+	void* shader_byte_code = nullptr;
+	size_t size_shader = 0;
+	GraphicsEngine::get()->compileVertexShader(L"VertexShader.hlsl", "vsmain", &shader_byte_code, &size_shader);
+
+	m_vs=GraphicsEngine::get()->createVertexShader(shader_byte_code, size_shader);
+	m_vb->load(list, sizeof(vertex), size_list, shader_byte_code, size_shader);
+
+	GraphicsEngine::get()->releaseCompiledShader();
+
+
+	GraphicsEngine::get()->compilePixelShader(L"PixelShader.hlsl", "psmain", &shader_byte_code, &size_shader);
+	m_ps = GraphicsEngine::get()->createPixelShader(shader_byte_code, size_shader);
+	GraphicsEngine::get()->releaseCompiledShader();
+
+	constant cc;
+	cc.m_angle = 0;
+
+	m_cb = GraphicsEngine::get()->createConstantBuffer();
+	m_cb->load(&cc, sizeof(constant));
+
+}
+
+void AppWindow::onUpdate()
+{
+	Window::onUpdate();
+	//limpa o alvo de renderizacao
+	GraphicsEngine::get()->getImmediateDeviceContext()->clearRenderTargetColor(this->m_swap_chain,
+		0, 0.3f,0.4f, 1);
+
+	RECT rc = this->getClientWindowRect();
+	GraphicsEngine::get()->getImmediateDeviceContext()->setViewportSize(rc.right - rc.left, rc.bottom - rc.top);
+
+	unsigned long new_time = 0;
+	if (m_old_time)
+		new_time = ::GetTickCount() - m_old_time;
+	m_delta_time = new_time / 1000.0f;
+	m_old_time = ::GetTickCount();
+
+	m_angle += 1.57f*m_delta_time;
+	constant cc;
+	cc.m_angle = m_angle;
+
+	m_cb->update(GraphicsEngine::get()->getImmediateDeviceContext(), &cc);
+
+	GraphicsEngine::get()->getImmediateDeviceContext()->setConstantBuffer(m_vs, m_cb);
+	GraphicsEngine::get()->getImmediateDeviceContext()->setConstantBuffer(m_ps, m_cb);
+
+	//define o shader padrao dentro do pipeline de grafico para conseguir desenhar o objeto
+	GraphicsEngine::get()->getImmediateDeviceContext()->setVertexShader(m_vs);
+	GraphicsEngine::get()->getImmediateDeviceContext()->setPixelShader(m_ps);
+
+
+	//define os vertices do triangulo a ser desenhado
+	GraphicsEngine::get()->getImmediateDeviceContext()->setVertexBuffer(m_vb);
+
+	// desenha o triangulo na tela e apresenta apos carregar na swap_chain
+	GraphicsEngine::get()->getImmediateDeviceContext()->drawTriangleStrip(m_vb->getSizeVertexList(), 0);
+	m_swap_chain->present(true);
+}
+
+//quando finaliza o app, limpa todos os componentes carregados anteriormente
+void AppWindow::onDestroy()
+{
+	Window::onDestroy();
+	m_vb->release();
+	m_swap_chain->release();
+	m_vs->release();
+	m_ps->release();
+	GraphicsEngine::get()->release();
+}
